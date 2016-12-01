ecabd296c4587ba88c90be5bc6b750773fd43551
yeoman <idanishraza@gmail.com>
2016-12-01 05:17:48 +0200

    removing sub folder and cleaning directory

--- withReact (copy)/app/components/forms/form_elements.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default class formEl extends React.Component {
-
-	render() {
-		return <div className={this.props.className}>{this.props.children}</div>;
-	}
-}
--- withReact (copy)/app/main.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/layout/App.js';
-
-ReactDOM.render(<App />, document.getElementById('root'));
