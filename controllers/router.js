b2a109fee3bca6fef4856089aa0c3bb3fda3c110
yeoman <idanishraza@gmail.com>
2016-12-01 10:25:04 +0200

    added promise for twilio sms

--- controllers/router.js
+++ controllers/router.js
@@ -2,11 +2,7 @@
 const express = require('express');
 const router = express.Router();
 const phone       =   require('phone');
-// var config = require('../config');
-// var client = require('twilio')(config.accountSid, config.authToken);
-// var twilio = require('twilio')
-// var client = new twilio.RestClient(config.accountSid, config.authToken);
-const twilioClient = require('../twilioClient');
+
 router.post('/api/sms-promotion', function(req, res) {
     let phoneNumber_array = phone(req.body.phone);
     let phoneNumber = phoneNumber_array[0];
